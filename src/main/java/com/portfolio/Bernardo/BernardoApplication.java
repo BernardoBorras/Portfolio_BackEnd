42e6e224294d406713597e5e8bf507c6e501bb51
Bernardo Borras <bernardo.borras@hotmail.com>
2023-04-01 18:19:01 -0300

    initial commit

---
+++ src/main/java/com/portfolio/Bernardo/BernardoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Bernardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BernardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BernardoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/Bernardo/BernardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Bernardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BernardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
